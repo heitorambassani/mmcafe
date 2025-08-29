5596db9f9f337296a5a5482ecdc294a6592ecbbf
Heitor Bassani <heitor.bassani.ext@dasa.com.br>
2025-08-29 13:49:34 -0300

    chore: ajustes README e configs

--- src/main/java/com/mmcafe/upload_image_ia/entity/TblDadosImage.java
+++ src/main/java/com/mmcafe/upload_image_ia/entity/TblDadosImage.java
@@ -7,13 +7,9 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tbl_dados_image")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data @NoArgsConstructor @AllArgsConstructor
 public class TblDadosImage {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 512)
@@ -22,15 +18,12 @@ public class TblDadosImage {
     @Column(length = 128)
     private String tipo;
 
-    @Lob
     @Column(columnDefinition = "TEXT")
     private String descricao;
 
-    @Lob
-    @Column(columnDefinition = "TEXT")
+    @Column(length = 2048)
     private String url;
 
-    @Lob
     @Column(columnDefinition = "TEXT")
     private String conteudo;
 }
